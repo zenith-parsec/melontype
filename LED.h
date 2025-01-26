9b294ee5c60a18ad2ef055da4c23a53878694724
zenith parsec <37597580+zenith-parsec@users.noreply.github.com>
2025-01-25 22:31:08 -0500

    This version is probably fine to use now. So was the previous version of this specific file, but you can't be too careful.

--- LED.h
+++ LED.h
@@ -1,7 +1,7 @@
 #ifndef __LED_H__
 #define __LED_H__
 #include <Arduino.h>
-
+// fake change so I can recommit it with impunity. Want to get rid of commit message only.
 void initLEDs();
 void setRGB(uint16_t r, uint16_t g, uint16_t b, uint8_t idx=0);
 void updateLEDs();
